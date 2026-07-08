fa5697df72b4602c60077f304e9440776109226d
Econexo <econexo.hub@gmail.com>
2026-07-08 17:06:23 -0400

    Create index.ts

---
+++ apapacha-mobile/src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { AppText } from './AppText';
+export { Button } from './Button';
+export { Chip } from './Chip';
+export { RatingStars } from './RatingStars';
+export { Avatar } from './Avatar';
+export { Banner } from './Banner';
+export { StatTile } from './StatTile';
+export { ScreenBackground } from './ScreenBackground';
